2aeb9447afe0a4028ade7a27f36ceb51ab760eb7
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2016-11-08 15:51:02 +0000

    Created service to generate random pirate name.

--- lib/badge_component.dart
+++ lib/badge_component.dart
@@ -1,26 +1,34 @@
 import 'package:angular2/core.dart';
+import 'name_service.dart';
 
 @Component(
     selector: 'pirate-badge',
     templateUrl: 'badge_component.html',
-    styleUrls: const ['badge_component.css']
-)
-
+    styleUrls: const ['badge_component.css'],
+    providers: const ['NameService'])
 class BadgeComponent {
+  final NameService _nameService;
   String badgeName = 'Callum';
   String buttonText = 'Aye! Gimme a name!';
   bool isButtonEnabled = true;
+  BadgeComponent(this._nameService);
   void generateBadge() {
-    badgeName = 'Anne Bonney';
+    setBadgeName();
   }
+
   void updateBadge(String inputName) {
-    badgeName = inputName;
-    if(inputName.trim().isEmpty) {
+    setBadgeName(inputName);
+    if (inputName.trim().isEmpty) {
       buttonText = "Aye! Gimme a name!";
       isButtonEnabled = true;
     } else {
       buttonText = 'Arrr! Write yer name!';
       isButtonEnabled = false;
     }
+  }
+
+  void setBadgeName([String newName = '']) {
+    if(newName == null) return;
+    badgeName = _nameService.getPirateName(newName);
   }
 }
